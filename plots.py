bc75899e98a374cecb73611207582192039e09fe
Marco <markfds01@gmail.com>
2024-08-21 11:40:34 +0200

    all communities and changed plot

--- plots.py
+++ plots.py
@@ -44,7 +44,7 @@ def plot_daily_switchpoints(data, start_date, end_date, trace, n_switchpoints,re
     dates = pd.date_range(start_date, end_date).strftime('%y-%m-%d')
     plot_dates = [dates[i] for i in range(0, len(posterior_quantile[2, :]), 21)]
 
-    plt.figure()
+    plt.figure(figsize=(12, 10))
     # Data
     plt.plot(
         dates, posterior_quantile[2, :],
@@ -89,6 +89,7 @@ def plot_daily_switchpoints(data, start_date, end_date, trace, n_switchpoints,re
     plt.xticks(plot_dates, rotation = 45)
     plt.ylabel('Daily number of admissions', fontsize='large',fontweight = 'bold')
     plt.xlabel('Day', fontsize='large',fontweight = 'bold')
+    plt.title(f'{region}',fontsize = 22, fontweight = 'bold')
 
     fontsize = 'medium'
     plt.legend(loc='upper left', fontsize=fontsize)
@@ -99,7 +100,8 @@ def plot_daily_switchpoints(data, start_date, end_date, trace, n_switchpoints,re
         label.set_fontweight('bold')
     for label in plt.gca().get_yticklabels():
         label.set_fontweight('bold')
-    plt.savefig(f'plots/fit_{region}_switchpoints_new.png')
+    plt.savefig(f'plots/fit_{region}_fixed_switchpoints.png')
+    plt.savefig(f'plots/fit_{region}_fixed_switchpoints_new.png')
 
 
 
