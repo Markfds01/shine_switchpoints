b239efe27ba602fcd41b98ddc58805803cc03a6d
Marco <markfds01@gmail.com>
2024-08-30 13:08:31 +0200

    changed paralel

--- paralelize.sh
+++ paralelize.sh
@@ -1,4 +1,4 @@
 declare -a array_deaths=("AN" "AR" "CL" "CM" "CT" "EX" "GA" "IB" "MC" "MD" "PV" "Spain" "VC")
 
 # Use parallel to run the jobs concurrently
-parallel -j 4 python3 main.py -r {} -ns 2 -sw ::: "${array_deaths[@]}"
+parallel -j 15 python3 main.py -r {} -ns 2 -sw -ag ::: "${array_deaths[@]}"
